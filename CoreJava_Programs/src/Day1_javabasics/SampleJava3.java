f68b34d51b6e417a47574ffdc9437d7e0f81ae39
sam <sam@DESKTOP-MGOADM6.mshome.net>
2024-03-05 16:26:11 +0530

    It is my modified program

--- CoreJava_Programs/src/Day1_javabasics/SampleJava3.java
+++ CoreJava_Programs/src/Day1_javabasics/SampleJava3.java
@@ -4,11 +4,13 @@ public class SampleJava3 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-        int a,b,c;
+        int a,b,c,d;
         System.out.println("Enter the values of a and b:");
         a=s.nextInt();
         b=s.nextInt();
         c=a+b;
         System.out.println("Addition of a="+a+" and b="+b+" is: "+c);
+        d=a-b;
+        System.out.println("Subtraction of a="+a+" and b="+b+" is: "+d);
 	}
 }
