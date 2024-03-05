a1ece75d7b1cea07067edd4d33e4a774917013ba
sam <sam@DESKTOP-MGOADM6.mshome.net>
2024-03-05 15:44:16 +0530

    It is my core java practical programs.

---
+++ CoreJava_Programs/src/Day1_javabasics/SampleJava.java
@@ -0,0 +1,11 @@
+package Day1_javabasics;
+
+public class SampleJava {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		System.out.println("By Sindhuja");
+         
+	}
+
+}
---
+++ CoreJava_Programs/src/Day1_javabasics/SampleJava2.java
@@ -0,0 +1,14 @@
+package Day1_javabasics;
+
+public class SampleJava2 {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=100;
+		b=150;
+		c=a+b;
+		System.out.println("Addition of a="+a+" and b="+b+" is: "+c);
+
+	}
+
+}
